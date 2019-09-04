44868062dc225962b522fcf22a524ac855152078
Sofia Vera <53977433+sofiavera24@users.noreply.github.com>
2019-09-04 18:04:17 -0300

    Login y registro

---
+++ php/login.php
@@ -0,0 +1,38 @@
+<html>
+
+  <?php include("menu.php") ?>
+  <?php include("head.php") ?>
+
+<head>
+  <link rel="stylesheet" href="style.css">
+    <meta charset="utf-8">
+    <title>LOGIN</title>
+</head>
+
+<body class="login">
+
+<header>
+
+
+</header>
+
+
+<form class="" action="practica.html" method="post">
+
+    <p class="formulario">
+      <input type="" name="nombre" value="" placeholder="nombre" required="">
+    </p>
+
+    <p class="formulario">
+      <input type="" name="apellido" value="" placeholder="apellido" required="">
+    </p>
+
+    <button type="reset" name="button">BORRAR</button>
+
+          <button class="boton" type="submit" name="button">ENVIAR</button>
+
+      </form>
+
+</body>
+
+</html>
---
+++ php/register.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+
+<?php include("menu.php") ?>
+
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+      <meta charset="utf-8">
+      <title>REGISTRO</title>
+</head>
+  <body>
+
+
+    <form class="" action="practica.html" method="post">
+
+        <p class="formulario">
+          <input type="" name="nombre" value="" placeholder="nombre" required="">
+        </p>
+
+        <p class="formulario">
+          <input type="" name="apellido" value="" placeholder="apellido" required="">
+        </p>
+
+        <p class="formulario">
+            <input type="email" name="email" value="" placeholder="email" required="">
+        </p>
+
+        <p class="formulario">
+            <input id="contraseña" type="password" name="contraseña" value="" placeholder="contraseña" required="">
+        </p>
+
+          <button class="boton" type="reset" name="button">BORRAR</button>
+          <button class="boton" type="submit" name="button">ENVIAR</button>
+
+
+        </form>
+
+
+  </body>
+</html>
