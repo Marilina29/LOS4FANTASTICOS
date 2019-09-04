fbcdbf4261b95d823cba7025b7143617524a14c5
Marilina <marilina_29@hotmail.com>
2019-09-04 11:35:02 -0300

    fuentes

--- php/head.php
+++ php/head.php
@@ -2,5 +2,7 @@
   <meta charset="utf-8">
   <title><?= $titulo ?></title>
   <link rel="stylesheet" href="style.css">
+  <link href="https://fonts.googleapis.com/css?family=Pacifico&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Barlow+Condensed:400,500&display=swap" rel="stylesheet">
   <script src="https://kit.fontawesome.com/62ba28ee55.js"></script>
 </head>
