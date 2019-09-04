f66fcbbd2860bc70df003502a226154b54d09b90
Fede <federicostefanini@gmail.com>
2019-09-04 09:41:30 -0300

    HEADER

--- php/head.php
+++ php/head.php
@@ -1,4 +1,6 @@
 <head>
   <meta charset="utf-8">
   <title><?= $titulo ?></title>
+  <link rel="stylesheet" href="style.css">
+  <script src="https://kit.fontawesome.com/62ba28ee55.js"></script>
 </head>
