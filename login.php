b3c0ecb65b521e7ead955c55d0829d5f42db52d7
flor-kierko <florkierkos@hotmail.com>
2019-09-12 23:07:32 -0300

    agregue validacion login

--- login.php
+++ login.php
@@ -1,5 +1,25 @@
-<?php 
+<?php
 include_once("php/funciones.php");
+$errores = [];
+$errorEmail = "";
+$errorPass = "";
+
+if($_POST){
+
+  $errores = validarLogin($_POST);
+
+  if(!$errores){
+    loguearUsuario();
+    header("Location:index.php"); exit;
+  } else {
+    if(isset($errores["email"])){
+      $errorEmail = $errores["email"];
+          }
+    if(isset($errores["pass"])){
+      $errorPass = $errores["pass"];
+    }
+  }
+}
 
  ?>
 
@@ -13,11 +33,15 @@ include_once("php/funciones.php");
   <div class="cabecera">
     LOGIN
   </div>
-  <form action="index.php" method="post">
+  <form action="login.php" method="post">
   <input class="email" type="email" name="email" placeholder="email">
-  <br>
+  <?php
+  if(isset($errores["email"])){
+   ?> <p class="errores"> <?php echo $errorEmail;}?> </p>
   <input class="email" type="password" name="pass" placeholder="contraseña">
-  <br>
+  <?php
+  if(isset($errores["pass"])){
+   ?> <p class="errores" <?php echo $errorPass;}?> </p>
   <button class="boton" type="reset" name="borrar">BORRAR</button>
   <button class="boton" type="submit" name="enviar">ENTRAR</button>
 <a href="register.php"> <div class="cabecera">REGISTRATE </div></a>
