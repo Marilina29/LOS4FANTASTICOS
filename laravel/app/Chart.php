c2131d62db5413b17c85d440b235f0e93603a38f
flor-kierko <florkierkos@hotmail.com>
2019-11-05 12:05:39 -0300

    agrego controllers y modelos

---
+++ laravel/app/Chart.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Chart extends Model
+{
+
+  protected $guarded = [];
+
+}
--- laravel/app/User.php
+++ laravel/app/User.php
@@ -15,9 +15,11 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    // protected $fillable = [
+    //     'name', 'email', 'password', 'surname', 'dni', 'tel', 'adress_id'
+    // ];
+
+    protected $guarded = [];
 
     /**
      * The attributes that should be hidden for arrays.
