1611037b96987763e7b7f0f978352aa79022016a
xeristal <xeristal@xeristal.local>
2019-11-06 23:33:22 -0300

    listo tablas

---
+++ laravel/database/migrations/2019_11_07_001530_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('code')->unique();
+            $table->string('description');
+            $table->string('color');
+            $table->integer('price');
+            $table->bigInteger('style')->unsigned();
+            $table->foreign('style')->references('id')->on('styles')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
