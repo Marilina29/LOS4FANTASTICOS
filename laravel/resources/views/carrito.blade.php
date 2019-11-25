3476f18a94572aad0fa4390638d374c7a3cd0e39
Marilina <Marula@BIGs-iMac.local>
2019-11-25 11:50:52 -0300

    carro inicio de blade

--- laravel/resources/views/carrito.blade.php
+++ laravel/resources/views/carrito.blade.php
@@ -2,28 +2,29 @@
 
 @section('content')
 
-<div>
-  <div class="conjuntoUCproductos">
-    <div class="">
+<div class="">
+  <div class="ultimasCreaciones">
+    <div class="listaCarro">
       @php
         $total = 0;
       @endphp
       @foreach ($orders as $order)
-        <div class="">
+        <div class="itemCarro">
           <h3>{{$order->name}}</h3>
           <p>Precio: {{$order->price}}</p>
-          <form class="" action="/sacarCarrito" method="post">
+          <form class="quitar" action="/sacarCarrito" method="post">
             {{ csrf_field() }}
             <input type="hidden" name="id" value="{{$order->id}}">
-            <button class="botonComprar" type="submit" name="button">QUITAR</button>
+            <button class="botonQuitar" type="submit" name="button">QUITAR</button>
           </form>
         </div>
+        <hr>
         @php
           $total += $order->price;
         @endphp
       @endforeach
     </div>
-
+<div class="cierreCarro">
     <div class="">
       <p>TOTAL: {{$total}}</p>
     </div>
@@ -34,6 +35,7 @@
       {{ csrf_field() }}
       <button class="botonComprar" type="submit" name="button">COMPRAR</button>
     </form>
+</div>
 </div>
 
 @endsection
